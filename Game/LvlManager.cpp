003a46007e6a07e5cb7bd64e29caf74ff1eecbbf
rp360fr <matthias@terrot.net>
2026-03-12 16:04:08 +0100

    h

--- Game/LvlManager.cpp
+++ Game/LvlManager.cpp
@@ -87,6 +87,19 @@ void createfloor(int x, int y, int length, int width, sf::Angle,Scene* scene) {
 	
 }
 
+void CreateTree(int x, int y, int length, int width, sf::Angle, Scene* scene) {
+
+    for (int i = width; i > 0; i--)
+    {
+        for (int j = length; j > 0; j--)
+        {
+            GameObject* tree = CreateTree(x + j, y + i);
+            scene->AddGameObject(tree,{x,y});
+        }
+    }
+
+}
+
 
 Scene* CreateGameLvL()
 {
@@ -104,7 +117,9 @@ Scene* CreateGameLvL()
     lvl1->AddGameObject(caillou, { 8,2 });
     lvl1->AddGameObject(caillou2, { 9,2 });
     lvl1->AddGameObject(arbe, { 2,2 });
+  
     createfloor(0, 0, 15, 100, sf::degrees(0), lvl1);
+    CreateTree(0, 1, 1, 100, sf::degrees(0), lvl1);
     lvl1->SetPlayer(player);
     lvl1->SetLvLData(lvlObject);
     return lvl1;
